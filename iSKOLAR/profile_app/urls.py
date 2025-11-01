528346832713b883ee6a20e0856d5ef35c62f418
kirito <benoliraoclyde71@gmail.com>
2025-11-01 20:56:06 +0800

    Added profile display page showing edited information and addded back arrow

--- iSKOLAR/profile_app/urls.py
+++ iSKOLAR/profile_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.profile_view, name="profile"),
     path('profile/view/', views.view_profile, name='view_profile'),
-    path("logout/", views.logout_view, name="logout"),
+    
 ]
