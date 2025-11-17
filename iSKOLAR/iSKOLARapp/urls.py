d445d3de0f680d140c2ef700bf12acdaef2acd3d
kirito <benoliraoclyde71@gmail.com>
2025-11-17 21:56:19 +0800

    Paste urls

--- iSKOLAR/iSKOLARapp/urls.py
+++ iSKOLAR/iSKOLARapp/urls.py
@@ -7,14 +7,4 @@ urlpatterns = [
     path("archives/", views.archives_view, name="archives"),
     path("saved_scholarships/", views.saved_scholarships_view, name="saved_scholarships"),
     path("applications/", views.applications_view, name="applications"),
-
-   
- #profile-display-feature
-    #path("search-posts/", views.search_posts_view, name="search_posts"),
-
-
-
-
-
-  
 ]
