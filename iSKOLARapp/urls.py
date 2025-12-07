8bc7d7fbf1808a164d93e7906e87f013060720cc
ReenahMae <rcampilanan.reenahc@gmail.com>
2025-12-07 17:46:59 +0800

    Restore code

--- iSKOLARapp/urls.py
+++ iSKOLARapp/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views  # if you keep forgot_view or others
 
+
 urlpatterns = [
     path("", views.iSKOLARapp_view, name="iSKOLARapp"),
     path("archives/", views.archives_view, name="archives"),
